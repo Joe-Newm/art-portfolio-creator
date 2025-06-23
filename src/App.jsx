c8c5a2400422d095864b811c8560b7470269919d
Joe-Newm <j.newm500@gmail.com>
2025-06-22 20:52:25 -0500

    add: base

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   return (
 
     <div className="flex flex-col min-h-screen">
-      <Router basename="/art-portfolio-creator">
+      <Router>
         <ScrollToTop />
         <main className="flex-grow">
         <AppRoutes />
